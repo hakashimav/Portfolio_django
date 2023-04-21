c52622f9a221c3a918296fc852064a7a91fa1dee
MichMav <michmav28@gmail.com>
2023-04-21 20:04:09 +0100

    add the templates

---
+++ Portfolio/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.template.response import TemplateResponse
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
+from django.http import Http404
+import os
+
+# Create your views here.
+def index(request):
+
+    context = {}
+
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
