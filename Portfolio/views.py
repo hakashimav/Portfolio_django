1d10728528e4bb74bd16ce55bfb9b6812976d6fb
MichMav <michmav28@gmail.com>
2023-04-21 20:14:11 +0100

    add the views for all page

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -12,3 +12,25 @@ def index(request):
 
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
+
+
+def about(request):
+    
+    context = {}
+
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render(context, request))
+
+def service(request):
+    
+    context = {}
+
+    template = loader.get_template('service.html')
+    return HttpResponse(template.render(context, request))
+
+def realisation(request):
+    
+    context = {}
+
+    template = loader.get_template('realisation.html')
+    return HttpResponse(template.render(context, request))
