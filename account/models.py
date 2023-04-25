9c9073904c9d9fcf702ac760e54c29051012a169
MichMav <michmav28@gmail.com>
2023-04-25 22:19:24 +0100

    fixed the admin

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-# class image_projet(models.Model):
-#     name = models.CharField(max_length=50, null=True, blank=True)
-#     image = models.ImageField(upload_to='media/')
+class image_projet(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField(upload_to='media/')
