b924dc9311cbe52a90690135985258bf69414683
MichMav <michmav28@gmail.com>
2023-04-25 18:20:30 +0100

    add the views for account

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+#from django.conf.urls import url, handler404
+from . import views
+
+urlpatterns = [
+    path('dashboard',views.dashboard,name='dashboard'),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def dashboard(request):
+    context = {
+        'welcome':"Bienvenue dans Administration de Mich - Mav Portfolio",
+    }
+
+    template = loader.get_template('dashboard.html')
+    return HttpResponse(template.render(context, request))
