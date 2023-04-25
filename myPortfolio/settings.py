98eea26c4430a5a4e1871bd6b66e8ec2cb9e0944
MichMav <michmav28@gmail.com>
2023-04-25 18:07:12 +0100

    add the root for media

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
@@ -126,3 +126,7 @@ STATICFILES_DIRS=(
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/myPortfolio/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
